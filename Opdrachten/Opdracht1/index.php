8d01bcdbc9232fcb643cd130927093ae247fa1c8
MarkBout <mbout.roc-dev.com>
2019-11-12 11:33:20 +0100

    Opdracht 2 gemaakt

--- Opdrachten/Opdracht1/index.php
+++ Opdrachten/Opdracht1/index.php
@@ -1,2 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>HomePage //Opdrachten & Hoofdstukken//</title>
+</head>
+<body>
+
 <?php
-echo "hello world";
+echo "<br>";
+echo '<a href="http://phpcrusus.coolpage.biz/Opdracht_2/Opdracht1.php">H002/opdracht1</a><br>';
+echo '<a href="http://phpcrusus.coolpage.biz/Opdracht_2/Opdracht2.php">H002/opdracht2</a><br>';
+echo '<a href="http://phpcrusus.coolpage.biz/Opdracht_2/Opdracht3.php">H002/opdracht3</a><br>';
+echo '<a href="http://phpcrusus.coolpage.biz/Opdracht_2/Opdracht4.php">H002/opdracht4</a><br>';
+?>
+</body>
+</html>
