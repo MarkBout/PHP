0e97eef05ad7bc07348ecfac9184cb7d2f85b9be
MarkBout <mbout.roc-dev.com>
2019-12-11 10:34:15 +0100

    Opdracht 3 H05

--- Opdrachten/H05/Opdracht3.php
+++ Opdrachten/H05/Opdracht3.php
@@ -1,21 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>H005/loginform_resultaat</title>
+</head>
+<body>
 <?php
-
-function checkCombination($name, $password){
-    //userdata om te bekijken
-    $email = array('piet@worldonline.nl', 'klaas@carpets.nl', 'truushendriks@wegweg.nl');
-    $wachtwoord = array('doetje123', 'snoepje777', 'arkiearkie201');
-    $combination;
-    //kijken of wat er is ingevoerd en wat in de arrays staat hetzelfde is
-    if((($name == $email[0]) && ($password == $wachtwoord[0]))
-        || (($name == $email[1]) && ($password == $wachtwoord[1]))
-        || (($name == $email[2]) && ($password == $wachtwoord[2]))){
-        echo "Welcome!";
-        $combination = true;
-    } else {
-        echo "Sorry, geen toegang!";
-        $combination = false;
-    }
-    return $combination;
+$email = array('piet@worldonline.nl', 'klaas@carpets.nl', 'truushendriks@wegweg.nl');
+$wachtwoord = array('doetje123', 'snoepje777', 'arkiearkie201');
+if((($_GET['email'] == $email[0]) && ($_GET['wachtwoord'] == $wachtwoord[0]))
+    || (($_GET['email'] == $email[1]) && ($_GET['wachtwoord'] == $wachtwoord[1]))
+    || (($_GET['email'] == $email[2]) && ($_GET['wachtwoord'] == $wachtwoord[2]))){
+    echo "Welcome!";
+} else {
+    echo "Sorry, geen toegang!";
 }
-
-echo "<br>" . checkCombination($_POST['email'], $_POST['wachtwoord']);
+?>
+</body>
+</html>
