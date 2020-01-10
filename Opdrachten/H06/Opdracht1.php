cb99649746307e9e15fb1103526f674ed0f39115
MarkBout <mbout.roc-dev.com>
2020-01-10 21:29:44 +0100

    H06 opdracht 1

---
+++ Opdrachten/H06/Opdracht1.php
@@ -0,0 +1,60 @@
+<?php
+
+try{
+    $dbc = new PDO('mysql:host=localhost;dbname=school;port=3306' , 'root', '');
+}catch (PDOException $e){
+    echo 'Connection failed: ' . $e->getMessage();
+    exit();
+}
+
+$query = "SELECT * FROM student";
+$result = $dbc->query($query) or die("Error querying");
+?>
+
+<style>
+    table{
+        border: 1px solid black;
+    }
+</style>
+
+<table>
+
+    <?php
+    echo '<tr>';
+    echo '<td> studentnr </td>';
+    echo '<td> roepnaam </td>';
+    echo '<td> voorletters </td>';
+    echo '<td> tussenvoegsels </td>';
+    echo '<td> achternaam </td>';
+    echo '<td> adres </td>';
+    echo '<td> postcode </td>';
+    echo '<td> woonplaats </td>';
+    echo '<td> geslacht </td>';
+    echo '<td> telefoon </td>';
+    echo '<td> geboortedatum </td>';
+    echo '<td> uitgeschreven </td>';
+    echo '<td> lesgeld </td>';
+    echo '<td> betaald </td>';
+    echo '</tr>';
+
+    while ($row = $result->fetch()){
+
+        echo '<tr>';
+        echo '<td>' . $row['studentnr'] . '</td>';
+        echo '<td>' . $row['roepnaam'] . '</td>';
+        echo '<td>' . $row['voorletters'] . '</td>';
+        echo '<td>' . $row['tussenvoegsels'] . '</td>';
+        echo '<td>' . $row['achternaam'] . '</td>';
+        echo '<td>' . $row['adres'] . '</td>';
+        echo '<td>' . $row['postcode'] . '</td>';
+        echo '<td>' . $row['woonplaats'] . '</td>';
+        echo '<td>' . $row['geslacht'] . '</td>';
+        echo '<td>' . $row['telefoon'] . '</td>';
+        echo '<td>' . $row['geboortedatum'] . '</td>';
+        echo '<td>' . $row['uitgeschreven'] . '</td>';
+        echo '<td>' . $row['schoolgeld'] . '</td>';
+        echo '<td>' . $row['betaald'] . '</td>';
+        echo '</tr>';
+    }
+    ?>
+</table>
